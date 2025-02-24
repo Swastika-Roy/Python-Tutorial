c33864b73b2b90a8090d04e7f7020fb81a0f150a
Swastika Roy <swastikaroytitu@gmail.com>
2025-02-24 18:58:36 +0530

    modified in calculater

--- my programs/Calculator.py
+++ my programs/Calculator.py
@@ -1,7 +1,8 @@
 a = int(input("Enter first value = "))
 b = int(input("Enter another value = "))
-print("division of  ",a," and ", b," is ",a/b)
+print("quotient of  ",a," and ", b," is ",a/b)
 print("subtraction of  ",a," and  ", b," is ",a-b)
 print("addition of  ",a," and ", b," is ",a+b)
 print("multiplication of  ",a," and  ", b," is ",a*b)
-
+print("remainder of  ",a," and ", b," is ",a%b)
+print("power of  ",a," and ", b," is ",a**b)
