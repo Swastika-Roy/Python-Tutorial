252ed9b9a724da1e9d42f661a4c73915490b5141
Swastika Roy <swastikaroytitu@gmail.com>
2025-02-27 23:20:59 +0530

    My new program of read() and readlines() method

---
+++ my programs/readAndreadlinesMethods.py
@@ -0,0 +1,13 @@
+f = open('myfile3.txt','r')
+i = 0
+while True:
+    i = i + 1
+    line = f.readline()
+    if not line:
+        break
+    m1 = line.split(",")[0]
+    m2 = line.split(",")[1]
+    m3 = line.split(",")[2]
+    print(f"marks of student {i} in Maths : {m1}")
+    print(f"marks of student {i} in Maths : {m2}")
+    print(f"marks of student {i} in Maths : {m3}")
