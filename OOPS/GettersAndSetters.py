84a6bb851efe6d988a9b0891d34f07912e43c885
Swastika Roy <swastikaroytitu@gmail.com>
2025-03-04 19:35:16 +0530

    GETTERS AND SETTERS IN PYTHON

---
+++ OOPS/GettersAndSetters.py
@@ -0,0 +1,24 @@
+class MyClass:
+
+    def __init__(self,value):
+        self.val = value
+
+
+    def show(self):
+        print(f"Value is {self.val}")
+
+
+    @property                   #getter
+    def ten_value(self):
+        return 10 * self.val
+
+
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self.val = new_value/10
+
+
+obj = MyClass(10)
+obj.ten_value = 67
+print(obj.ten_value)
+obj.show()
