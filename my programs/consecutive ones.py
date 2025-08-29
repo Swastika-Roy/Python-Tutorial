da0f100c5920f51dff05d9fefe94eff176c45289
Swastika Roy <swastikaroytitu@gmail.com>
2025-08-29 19:48:15 +0530

    Without comment

--- my programs/consecutive ones.py
+++ my programs/consecutive ones.py
@@ -1,19 +1,18 @@
 def maxOnes(arr, k):
     res = 0
 
-    # Start and end pointer of the window
+
     start = 0
     end = 0
 
-    # Counter to keep track of zeros in current window
+
     cnt = 0
 
     while end < len(arr):
         if arr[end] == 0:
             cnt += 1
 
-        # Shrink the window from left if no.
-        # of zeroes are greater than k
+
         while cnt > k:
             if arr[start] == 0:
                 cnt -= 1
@@ -22,8 +21,7 @@ def maxOnes(arr, k):
 
         res = max(res, (end - start + 1))
 
-        # Increment the end pointer
-        # to expand the window
+
         end += 1
 
     return res
