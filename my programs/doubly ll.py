3a5e2730357398c3653c628c44bc8711504bb451
Swastika Roy <swastikaroytitu@gmail.com>
2025-09-03 10:10:43 +0530

    doubly ll

---
+++ my programs/doubly ll.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, new_data):
+        self.data = new_data
+        self.next = None
+        self.prev = None
+
+
+def reverse(head):
+    # If the list is empty or has only one node,
+    # return the head as is
+    if head is None or head.next is None:
+        return head
+
+    prevNode = None
+    currNode = head
+
+    # Traverse the list and reverse the links
+    while currNode is not None:
+        # Swap the next and prev pointers
+        prevNode = currNode.prev
+        currNode.prev = currNode.next
+        currNode.next = prevNode
+
+        # Move to the next node in the original list
+        # (which is now previous due to reversal)
+        currNode = currNode.prev
+
+    return prevNode.prev
+
+
+def printList(node):
+    while node is not None:
+        print(node.data, end="")
+        if node.next is not None:
+            print(" <-> ", end="")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.prev = head
+    head.next.next = Node(3)
+    head.next.next.prev = head.next
+
+    head = reverse(head)
+    printList(head)
