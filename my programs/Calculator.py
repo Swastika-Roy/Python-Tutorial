abf84428fc03b28557325698a33fb9244916226d
Swastika-Roy <swastikaroytitu@gmail.com>
2026-01-04 21:42:41 +0530

    Update Calculator.py

--- my programs/Calculator.py
+++ my programs/Calculator.py
@@ -1,10 +1,10 @@
 a = int(input("Enter first value = "))
 b = int(input("Enter another value = "))
-print("quotient of  ",a," and ", b," is ",a/b)
-print("subtraction of  ",a," and  ", b," is ",a-b)
-print("addition of  ",a," and ", b," is ",a+b)
-print("multiplication of  ",a," and  ", b," is ",a*b)
-print("remainder of  ",a," and ", b," is ",a%b)
-print("power of  ",a," and ", b," is ",a**b)
+print("quotient of  ",a," and ", b," =",a/b)
+print("subtraction of  ",a," and  ", b," = ",a-b)
+print("addition of  ",a," and ", b," = ",a+b)
+print("multiplication of  ",a," and  ", b," = ",a*b)
+print("remainder of  ",a," and ", b," = ",a%b)
+print("power of  ",a," and ", b," = ",a**b)
 
 
